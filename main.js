abbb1e786295183c1ae27398fbf179fe7af92db1
faniceHuang <fanice0721@gmail.com>
2024-02-04 17:29:02 +0800

    Render candlestick charts using canvas.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const binanceData = require('./binance/klineData.js');
+const tools = require("./tools");
 
 binanceData.KlineData("api3.binance.com", "ETHUSDT","1h");
+tools.drawCandlistick()
--- tools.js
+++ tools.js
@@ -1,1 +1,18 @@
-const { createCanvas, loadImage } = require('canvas')
+const { createCanvas } = require('canvas')
+const fs = require('fs')
+
+async function DrawCandlistick() {
+    const canvas = createCanvas(800,600)
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'green'
+    ctx.fillRect(100, 100, 50, 200)
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(100, 150,50,50)
+
+    const buffer = canvas.toBuffer('image/png')
+    fs.writeFileSync('candlestick.png', buffer)
+}
+
+module.exports = {DrawCandlistick:DrawCandlistick}
