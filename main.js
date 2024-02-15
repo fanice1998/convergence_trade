be00f0335ac393478be37b957cf5753d5655d77b
fanice1998 <fanice0721@gmail.com>
2024-02-15 09:25:54 +0800

    update

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const binanceData = require('./binance/klineData.js');
-// const tools = require("./tools/tools");
+const backTrader = require('./backTrader/backTrader.js');
 
-symbols = ["ETHUSDT", "OPUSDT", "BTCUSDT", "SUIUSDT"]
-for (const symbol of symbols) {
-    binanceData.KlineData("api3.binance.com", symbol, "1h")
-}
-// binanceData.KlineData("api3.binance.com", "ETHUSDT","1h");
-// tools.DrawCandlistick()
+
+
